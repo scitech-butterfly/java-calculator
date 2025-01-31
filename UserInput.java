5ac51d307a62ae3d21b44b279dd6796dad785ab7
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-01-31 23:26:40 +0530

    Update UserInput.java
    Add method to get array input based on user's choice of array size

--- UserInput.java
+++ UserInput.java
@@ -13,4 +13,16 @@ class UserInput {
         return numbers;
     }
 
+    // Method to get an array of integers from the user
+    int[] getArray() {
+        System.out.println("Enter the size of the array:");
+        int size = scanner.nextInt();
+        int[] array = new int[size];
+
+        System.out.println("Enter the elements of the array:");
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        return array;
+    }
 }
