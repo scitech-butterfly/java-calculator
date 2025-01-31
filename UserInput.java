a1f101479156e8c38a804701f70a3d8c431bb410
Kashvi Singh <127826843+scitech-butterfly@users.noreply.github.com>
2025-01-31 23:00:30 +0530

    Update UserInput.java
    Method to input 2 numbers from user

--- UserInput.java
+++ UserInput.java
@@ -1,1 +1,16 @@
 //UserInput.java
+
+class UserInput {
+    Scanner scanner = new Scanner(System.in);
+
+    // Method to get two integers from the user
+    int[] getTwoNumbers() {
+        int[] numbers = new int[2];
+        System.out.println("Enter first number:");
+        numbers[0] = scanner.nextInt();
+        System.out.println("Enter second number:");
+        numbers[1] = scanner.nextInt();
+        return numbers;
+    }
+
+}
